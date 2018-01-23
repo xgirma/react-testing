058a66e392c12c0589e482e52dbd046e687f97b4
xgirma <xgirma@gmail.com>
2018-01-23 01:44:50 -0800

    add sanpshot test

---
+++ src/__tests__/components/ExpenseDashboard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+// import ExpenseDashboardPage from '../../../src/components/ExpenseDashboardPage';
+
+// TODO resolve this error
+test.skip('should render ExpenseDashboardPage correctly', () => {
+	// const wrapper = shallow(<ExpenseDashboardPage/>);
+	// expect(wrapper).toMatchSnapshot();
+});
